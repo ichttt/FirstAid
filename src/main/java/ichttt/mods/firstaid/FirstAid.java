ed77a1541e45916e1d5d9b6f9d0897890c31ea35
Tobias Hotz <tobias.hotz@hotmail.de>
2018-02-01 17:50:33 +0100

    Bump version

--- src/main/java/ichttt/mods/firstaid/FirstAid.java
+++ src/main/java/ichttt/mods/firstaid/FirstAid.java
@@ -38,7 +38,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
 
-@Mod(modid = FirstAid.MODID, name = FirstAid.NAME, version = "1.4.6", acceptedMinecraftVersions = "[1.12.2,1.13)", dependencies = "required-after:forge@[14.23.0.2526,);")
+@Mod(modid = FirstAid.MODID, name = FirstAid.NAME, version = "1.4.7", acceptedMinecraftVersions = "[1.12.2,1.13)", dependencies = "required-after:forge@[14.23.0.2526,);")
 public class FirstAid {
     public static Logger logger;
     public static final String MODID = "firstaid";

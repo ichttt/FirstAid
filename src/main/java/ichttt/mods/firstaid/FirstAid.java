bbf7889348c5a017e8f89ae771459e2853f7f6e2
Tobias Hotz <tobias.hotz@hotmail.de>
2018-03-05 15:49:42 +0100

    Bump version to 1.5.1 for release

--- src/main/java/ichttt/mods/firstaid/FirstAid.java
+++ src/main/java/ichttt/mods/firstaid/FirstAid.java
@@ -46,7 +46,7 @@ public class FirstAid {
     public static Logger logger;
     public static final String MODID = "firstaid";
     public static final String NAME = "First Aid";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
 
     public static boolean isSynced = false;
 

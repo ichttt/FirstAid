d0d779ee53e4e5d631ce8c010b314d691f749849
Tobias Hotz <tobias.hotz@hotmail.de>
2019-11-19 16:08:18 +0100

    Fixup new class

--- src/main/java/ichttt/mods/firstaid/common/network/MessageUpdatePart.java
+++ src/main/java/ichttt/mods/firstaid/common/network/MessageUpdatePart.java
@@ -1,3 +1,21 @@
+/*
+ * FirstAid
+ * Copyright (C) 2017-2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ichttt.mods.firstaid.common.network;
 
 import ichttt.mods.firstaid.api.CapabilityExtendedHealthSystem;
@@ -17,12 +35,13 @@ public class MessageUpdatePart implements IMessage {
     private float absorption;
     private float currentHealth;
 
+    public MessageUpdatePart() {}
+
     public MessageUpdatePart(AbstractDamageablePart part) {
         this.id = part.part.id;
         this.maxHealth = part.getMaxHealth();
         this.absorption = part.getAbsorption();
         this.currentHealth = part.currentHealth;
-        validate();
     }
 
     @Override
@@ -40,6 +59,7 @@ public class MessageUpdatePart implements IMessage {
         buf.writeInt(maxHealth);
         buf.writeFloat(absorption);
         buf.writeFloat(currentHealth);
+        validate();
     }
 
     private void validate() {

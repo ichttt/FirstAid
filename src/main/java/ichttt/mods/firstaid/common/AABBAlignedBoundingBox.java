c4faf90c2fecc31aa582d45a8bb75f45b9d78537
Tobias Hotz <tobias.hotz@hotmail.de>
2021-03-18 20:23:47 +0100

    Add missing file AABBAlignedBoundingBox

---
+++ src/main/java/ichttt/mods/firstaid/common/AABBAlignedBoundingBox.java
@@ -0,0 +1,34 @@
+package ichttt.mods.firstaid.common;
+
+import net.minecraft.util.math.AxisAlignedBB;
+
+public class AABBAlignedBoundingBox {
+    private final double minX;
+    private final double minY;
+    private final double minZ;
+    private final double maxX;
+    private final double maxY;
+    private final double maxZ;
+
+    public AABBAlignedBoundingBox(double minX, double minY, double minZ, double maxX, double maxY, double maxZ) {
+        this.minX = minX;
+        this.minY = minY;
+        this.minZ = minZ;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.maxZ = maxZ;
+    }
+
+    public AxisAlignedBB createAABB(AxisAlignedBB original) {
+        double sizeX = original.getXsize();
+        double sizeY = original.getYsize();
+        double sizeZ = original.getZsize();
+        double newMinX = original.minX + (sizeX * minX);
+        double newMinY = original.minY + (sizeY * minY);
+        double newMinZ = original.minZ + (sizeZ * minZ);
+        double newMaxX = original.minX + (sizeX * maxX);
+        double newMaxY = original.minY + (sizeY * maxY);
+        double newMaxZ = original.minZ + (sizeZ * maxZ);
+        return new AxisAlignedBB(newMinX, newMinY, newMinZ, newMaxX, newMaxY, newMaxZ);
+    }
+}
